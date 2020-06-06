0ecf4dc77e669a9fb5a6b843363ca0a32e613abb
subranil <subranil.saha@tarento.com>
2020-06-06 21:11:30 +0530

    Issue #30DaysKotlin: added layout for details and main

---
+++ app/src/main/java/com/subranil/covid19traker/StateDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.subranil.covid19traker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StateDetailsActivity: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
